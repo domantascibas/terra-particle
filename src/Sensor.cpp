244c9bf04237fc2bd39bb51320855992b2abe4c6
Domantas Cibas <domantas@nectarsun.com>
2017-11-27 01:22:13 +0200

    improves the sensor class

--- src/Sensor.cpp
+++ src/Sensor.cpp
@@ -1,14 +1,47 @@
 #include "Sensor.h"
 
-Sensor::Sensor(uint8_t pin, uint8_t type, String id) : dht(pin, type) {
+Sensor::Sensor(uint8_t pin, uint8_t type, String id) : dht(pin, type), updater(5000, &Sensor::update, *this) {
   sensorType = type;
-  extrnalId = id;
+  externalId = id;
+  present = false;
+  newDataAvailable = false;
+  temperature = 0.00;
+  humidity = 0.00;
+}
+
+void Sensor::init() {
+  dht.begin();
+  updater.start();
+}
+
+void Sensor::update() {
+  __disable_irq();
+  temperature = dht.getTempCelcius();
+  humidity = dht.getHumidity();
+  if(!isnan(temperature) && !isnan(humidity)) present = true;
+  else present = false;
+  newDataAvailable = true;
+  __enable_irq();
 }
 
 float Sensor::getTemperature() {
-  return dht.getTempCelcius();
+  __disable_irq();
+  if(newDataAvailable) newDataAvailable = false;
+  __enable_irq();
+  return temperature;
 }
 
 float Sensor::getHumidity() {
-  return dht.getHumidity();
+  __disable_irq();
+  if(newDataAvailable) newDataAvailable = false;
+  __enable_irq();
+  return humidity;
+}
+
+bool Sensor::isPresent() {
+  return present;
+}
+
+bool Sensor::isNewDataAvailable() {
+  return newDataAvailable;
 }
--- src/Sensor.h
+++ src/Sensor.h
@@ -6,14 +6,23 @@
 
 struct Sensor {
   Sensor(uint8_t, uint8_t, String);
-  void start();
+  void init();
+  bool isPresent();
+  bool isNewDataAvailable();
   float getTemperature();
   float getHumidity();
 
   private:
+  void update();
+  float temperature;
+  float humidity;
+  bool present;
+  bool newDataAvailable;
+  bool sensorBusy;
   uint8_t sensorType;
   String externalId;
   DHT dht;
+  Timer updater;
 };
 
 #endif
