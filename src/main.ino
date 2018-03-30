d4cf6f6445c6528078312687911c5c3bcd8c6ed1
Domantas Cibas <domantas@nectarsun.com>
2018-03-30 21:56:33 +0300

    updates comments

--- src/main.ino
+++ src/main.ino
@@ -13,22 +13,26 @@ Project parts:
 
 */
 
+/*
+Index number gives the left shift amount (0x01 << index);
+          1 2 3 4 5 6 7 8
+Hour      [][][][][][][][]  int
+Minute    [][][][][][][][]  int
+NextState [][][][][][][][]  bool
+*/
+
 /* Consts */
 // BMP180 address 0x77
 static const uint8_t TIME_ZONE = 3;
 static const uint16_t LCD_REFRESH_INTERVAL = 1000;
 
+/* Object instances */
 LcdController lcdController(LCD_REFRESH_INTERVAL);
 
-/*
-          1 2 3 4 5 6 7 8
-Hour      [][][][][][][][]  int
-Minute    [][][][][][][][]  int
-NextState [][][][][][][][]  1/0
+/* Function definitions */
 
-Index number gives the left shift amount (0x01 << index);
-*/
 
+/* Function implementations */
 void setup(void) {
   Time.zone(TIME_ZONE);
   lcdController.init();
