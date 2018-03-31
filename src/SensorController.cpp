9a9e3e60a26333efd3427c466bda9cd26e4e8ac2
Domantas Cibas <domantas@nectarsun.com>
2018-03-31 23:51:44 +0300

    cleans up the sensor controller implementation

--- src/SensorController.cpp
+++ src/SensorController.cpp
@@ -8,20 +8,16 @@
   Sensor 4: inside the carcass above the mid-light
  */
 
-// Sensor s1(TEMP1, DHT22, "1UD86RVLUEi85Tv04hmAOhgg06IS");
-// Sensor s2(TEMP1, DHT22, "74Ra78275x4Im3v88Vy3038U5gq1");
-// Sensor s3(TEMP1, DHT11, "hD3oT6kuJoVINCSzLL34V2z3v0T6");
-// Sensor s4(TEMP1, DHT11, "");
-
-/* Consts */
 // Three DHT22 sensors connected to pins A4-6
 // One DHT11 sensor connected to pin A7
+
+/* Consts */
 static const uint8_t SHELF_SENSOR_ADDRESS = A4;
 static const uint8_t FLOOR_SENSOR_ADDRESS = A5;
 static const uint8_t BRANCH_SENSOR_ADDRESS = A6;
 static const uint8_t CEILING_SENSOR_ADDRESS = A7;
-static const uint8_t NUMBER_OF_SENSORS = 2;
-static const uint16_t SENSOR_REFRESH_INTERVAL = 5000;
+static const uint8_t NUMBER_OF_SENSORS = 1;
+static const uint16_t SENSOR_REFRESH_INTERVAL = 60000;
 
 /* Global variables */
 float temperatureArray[NUMBER_OF_SENSORS];
@@ -31,7 +27,7 @@ float humidityArray[NUMBER_OF_SENSORS];
 DHT shelfSensor(SHELF_SENSOR_ADDRESS, DHT22);
 // Sensor floorSensor(FLOOR_SENSOR_ADDRESS, DHT22);
 // Sensor branchSensor(BRANCH_SENSOR_ADDRESS, DHT22);
-// Sensor ceilingSensor(CEILING_SENSOR_ADDRESS, DHT22);
+// Sensor ceilingSensor(CEILING_SENSOR_ADDRESS, DHT11);
 
 /* Public functions */
 /* Constructor */
