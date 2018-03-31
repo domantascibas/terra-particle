ff92142f9af94d382ba7a7a6c4120b94035025e4
Domantas Cibas <domantas@nectarsun.com>
2018-03-31 23:42:31 +0300

    strips down Sensor implementation to bare essentials

--- src/Sensor.cpp
+++ src/Sensor.cpp
@@ -1,43 +1,36 @@
 #include "Sensor.h"
 
-Sensor::Sensor(uint8_t pin, uint8_t type, String id) : dht(pin, type), updater(5000, &Sensor::update, *this) {
+Sensor::Sensor(uint8_t pin, uint8_t type) : dht(pin, type) {
   sensorType = type;
-  externalId = id;
   present = false;
-  newDataAvailable = false;
-  temperature = 0.00;
-  humidity = 0.00;
 }
 
 void Sensor::init() {
   dht.begin();
-  updater.start();
 }
 
-void Sensor::update() {
-  __disable_irq();
-  temperature = dht.getTempCelcius();
-  humidity = dht.getHumidity();
-  if(!isnan(temperature) && !isnan(humidity)) present = true;
-  else present = false;
-  newDataAvailable = true;
-  __enable_irq();
-}
+// void Sensor::update() {
+//   __disable_irq();
+//   temperature = dht.getTempCelcius();
+//   humidity = dht.getHumidity();
+//   if(!isnan(temperature) && !isnan(humidity)) present = true;
+//   else present = false;
+//   newDataAvailable = true;
+//   __enable_irq();
+// }
 
 float Sensor::getTemperature() {
-  if(newDataAvailable) newDataAvailable = false;
-  return temperature;
+  return dht.getTempCelcius();
 }
 
 float Sensor::getHumidity() {
-  if(newDataAvailable) newDataAvailable = false;
-  return humidity;
+  return dht.getHumidity();
 }
 
 bool Sensor::isPresent() {
   return present;
 }
 
-bool Sensor::isNewDataAvailable() {
-  return newDataAvailable;
-}
+// bool Sensor::isNewDataAvailable() {
+//   return newDataAvailable;
+// }
--- src/Sensor.h
+++ src/Sensor.h
@@ -1,38 +1,29 @@
 #ifndef SENSOR_H
 #define SENSOR_H
 
-#include "Particle.h"
 #include "Adafruit_DHT.h"
 
 struct Sensor {
   /* Constructor */
-  Sensor(uint8_t, uint8_t, String);
+  Sensor(uint8_t, uint8_t);
 
   /* Public variables */
 
   /* Public functions */
   void init();
   bool isPresent();
-  bool isNewDataAvailable();
   float getTemperature();
   float getHumidity();
 
   private:
   /* Private objects */
   DHT dht;
-  Timer updater;
 
   /* Private variables */
-  float temperature;
-  float humidity;
   bool present;
-  bool newDataAvailable;
-  bool sensorBusy;
   uint8_t sensorType;
-  String externalId;
   
   /* Private functions */
-  void update();
 };
 
 #endif
